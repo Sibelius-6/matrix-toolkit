c4f09be96245b159b03f64b7b74754b676e9a5d9
Lechuan Peng <l33peng@uwaterloo.ca>
2018-12-04 23:09:58 -0500

    trial2

--- sqrt_trial.cc
+++ sqrt_trial.cc
@@ -7,10 +7,11 @@ typedef size_t radicand;
 typedef int multiple;
 
 class Sqrt {
+public:
   int rational;
   map<radicand, multiple> irrational;
   // = rational + multiple * √radicand
-public:
+
   Sqrt(int rational, map<radicand, multiple> irrational):
     rational{rational}, irrational{irrational} {}
 // simplify the expression in the ctor
@@ -27,13 +28,29 @@ public:
     }
     return rat + irr;
   } // √ = options + v
+
+  Sqrt operator+(const Sqrt &other) {
+    map<radicand, multiple> res_i = irrational;
+    for (const auto &p : other.irrational) {
+      if (res_i.count(p.first)) res_i.at(p.first) += p.second;
+      else res_i.insert(pair<radicand, multiple>(p.first, p.second));
+    }
+    return { rational + other.rational, res_i };
+  }
 };
 
 int main() {
   map<radicand, multiple> i {
     {2, 3}, {10, 1}, {8, -1}, {3, 102}, {23, 0}, {29, -102}
   };
-  Sqrt six {-10, i};
-  cout << six.pretty_print() << endl;
+  map<radicand, multiple> j {
+    {2, 3}, {10, -1}, {8, -10}, {7, 102}, {23, 0}, {22, -102}
+  };
+  Sqrt a {-10, i};
+  cout << "a is " << a.pretty_print() << endl;
+  Sqrt b {0, j};
+  cout << "b is " << b.pretty_print() << endl;
+  Sqrt c = a + b;
+  cout << "c is " << c.pretty_print() << endl;
 }
 
