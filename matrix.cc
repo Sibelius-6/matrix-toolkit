96e1e7e528208ad8599b98512ac78439057ac40e
Lechuan Peng <l33peng@uwaterloo.ca>
2019-01-05 14:40:55 -0500

    optimize the model

--- matrix.cc
+++ matrix.cc
@@ -1,1 +1,16 @@
-...
+#include <vector>
+#include <string>
+#include <memory>
+#include "complex.h"
+#include "matrix.h"
+
+using namespace std;
+
+// By default, all the entries are filled with 0
+Matrix::Matrix(int row, int col): row{row}, col{col} {
+  vector<unique_ptr<Complex>> row_of_zeros (col, make_unique<Complex>());
+  vector<vector<unique_ptr<Complex>>> all_zero (row, row_of_zeros);
+  data = move(all_zero);
+}
+
+
--- matrix.h
+++ matrix.h
@@ -2,15 +2,18 @@
 #define _MATRIX_H_
 
 #include <vector>
-
+#include <memory>
 class Complex;
 
 class Matrix {
   int row, col; // this should be consistent with the data
-  std::vector<std::vector<unique_ptr<Complex>>> data;
+  std::vector<std::vector<std::unique_ptr<Complex>>> data;
 
 public:
   Matrix(int row, int col);
+
+  void set_ij(int i, int j);
+  std::unique_ptr<Complex> get_ij(int i, int j);
     
   int getR() const { return row; }
   int getC() const { return col; }
